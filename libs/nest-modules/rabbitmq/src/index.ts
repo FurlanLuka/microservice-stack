5c26b8a24975ce66179693949bdd1d892ccdda53
Luka Furlan <100034176+FurlanLuka@users.noreply.github.com>
2023-01-20 13:08:28 +0100

    Added Event object export

--- libs/nest-modules/rabbitmq/src/index.ts
+++ libs/nest-modules/rabbitmq/src/index.ts
@@ -5,6 +5,7 @@ export * from './lib/rabbitmq-exchange.util';
 export * from './lib/rabbitmq.interfaces';
 export * from './lib/rabbitmq.module';
 export * from './lib/rabbitmq.service';
+export * from './lib/event/event';
 
 export const DEFAULT_EXCHANGE: RabbitMQExchangeConfig = {
   name: 'default',
