d88454a8d8e71e9d9ac3d816ca2fe270c6801947
Luka Furlan <100034176+FurlanLuka@users.noreply.github.com>
2023-01-25 12:54:19 +0100

    Added update ingress command to cli

--- libs/local-deployment/src/deploy-ingress-controller.ts
+++ libs/local-deployment/src/deploy-ingress-controller.ts
@@ -1,20 +1,28 @@
 import { exec } from 'child_process';
 import { promisify } from 'util';
-import { sleep } from './utils';
 
 const execAsync = promisify(exec);
 
-export async function deployIngressController(debugEnabled: boolean) {
+export async function enableIngressController(debugEnabled: boolean) {
   try {
     console.log('Enabling ingress controller...');
     await execAsync('minikube addons enable ingress');
-    await sleep(20000);
     console.log('Ingress controller enabled ✅');
+  } catch (error) {
+    if (debugEnabled) {
+      console.error(error);
+    }
+    throw new Error('Error while enabling ingress controller');
+  }
+}
 
+export async function deployIngressController(debugEnabled: boolean) {
+  try {
     console.log('Installing ingress controller');
     await execAsync(
       'helm upgrade --install ingress infrastructure/charts/ingress-controller --values infrastructure/ingress-values.yaml --set environment=local'
     );
+    console.log('Installing ingress controller deployed ✅');
   } catch (error) {
     if (debugEnabled) {
       console.error(error);
--- libs/local-deployment/src/index.ts
+++ libs/local-deployment/src/index.ts
@@ -3,22 +3,23 @@ import { program } from 'commander';
 import { checkRequirements } from './check-deployment-requirements';
 import { setupCluster } from './setup-cluster';
 import { deployService, deployServices } from './deploy-services';
-import { deployIngressController } from './deploy-ingress-controller';
+import {
+  deployIngressController,
+  enableIngressController,
+} from './deploy-ingress-controller';
 
 program
   .command('install')
   .alias('i')
   .option('-d, --debug')
   .action(async ({ debug = false }) => {
-    console.log(
-      chalk.bold('Starting local microservice deployment...')
-    );
+    console.log(chalk.bold('Starting local microservice deployment...'));
 
     try {
       await checkRequirements();
       await setupCluster(debug);
       await deployServices(debug);
-      await deployIngressController(debug);
+      await enableIngressController(debug);
     } catch (error) {
       console.log(chalk.bold.red(error.message));
     }
@@ -30,13 +31,25 @@ program
   .requiredOption('-s, --service <service>', 'service that should be upgraded')
   .option('-d, --debug')
   .action(async ({ service, debug = false }) => {
-    console.log(
-      chalk.bold('Starting local microservice deployment...')
-    );
+    console.log(chalk.bold('Starting local microservice deployment...'));
 
     try {
       await checkRequirements();
       await deployService(service, debug);
+    } catch (error) {
+      console.log(chalk.bold.red(error.message));
+    }
+  });
+
+program
+  .command('update-ingress')
+  .alias('ui')
+  .option('-d, --debug')
+  .action(async ({ debug = false }) => {
+    console.log(chalk.bold('Starting local microservice deployment...'));
+
+    try {
+      await checkRequirements();
       await deployIngressController(debug);
     } catch (error) {
       console.log(chalk.bold.red(error.message));
