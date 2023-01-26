5b586b85c53c9d0faefce152779fea633a30ade9
Luka Furlan <100034176+FurlanLuka@users.noreply.github.com>
2023-01-26 14:45:26 +0100

    Updated package lock

--- libs/workspace/src/utils/package-versions.ts
+++ libs/workspace/src/utils/package-versions.ts
@@ -1,6 +1,6 @@
 export const TYPEORM_VERSION = '0.3.11';
 export const PG_VERSION = '8.8.0';
 export const NESTJS_TYPEORM_VERSION = '9.0.1';
-export const MICROSERVICE_STACK_VERSION = '0.1.48';
+export const MICROSERVICE_STACK_VERSION = '0.1.49';
 export const CLASS_TRANSFORMER_VERSION = '0.5.1';
 export const CLASS_VALIDATOR_VERSION = '0.14.0';
