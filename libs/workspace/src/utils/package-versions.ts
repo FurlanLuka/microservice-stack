455bbb15b3ca7304998991d3ecfd44d13d03f407
Luka <lukafurlan.dev@gmail.com>
2023-06-30 09:44:29 +0200

    Updated microservice stack version to next

--- libs/workspace/src/utils/package-versions.ts
+++ libs/workspace/src/utils/package-versions.ts
@@ -1,6 +1,6 @@
 export const TYPEORM_VERSION = '0.3.17';
 export const PG_VERSION = '8.11.1';
 export const NESTJS_TYPEORM_VERSION = '10.0.0';
-export const MICROSERVICE_STACK_VERSION = '0.1.55';
+export const MICROSERVICE_STACK_VERSION = '0.1.56';
 export const CLASS_TRANSFORMER_VERSION = '0.5.1';
 export const CLASS_VALIDATOR_VERSION = '0.14.0';
